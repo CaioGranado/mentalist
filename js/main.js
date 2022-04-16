2433f643c020e1aa9e328b3061f51a1309b36816
Caio Granado <100639041+CaioGranado@users.noreply.github.com>
2022-04-15 21:01:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,14 +9,17 @@ function show (text) {
 }
 var secret = parseInt(Math.random () * 10);
 var input = document.querySelector("input");
+input.focus();
 
 function verife() {
     if(input.value == secret) {
-        alert("WOW, you discover the secret, the number is " + secret);
+        alert("WOW, you're a great mentalist, the magic number really is " + secret);
     } else {
-        alert("Sorry, you choice the wrong number :/");
+        alert("Sorry, you choose the wrong number :/");
         alert("The right number is " + secret);
     }
+    input.value = "";
+    input.focus();
 }
 
 var button = document.querySelector("button");
